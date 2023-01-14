07bd092e811171fe9f8f6db0d7f4bb36d225912a
srdjan <srdjan.gr@gmail.com>
2023-01-14 17:08:30 +0100

    Play/Pause buitton render

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import { FaForward, FaBackward, FaPlay, FaVolumeUp, FaVolumeMute } from 'react-icons/fa';
+import { useState, useEffect } from 'react'
+import { FaForward, FaBackward, FaPlay, FaVolumeUp, FaVolumeMute, FaPause } from 'react-icons/fa';
 
 import album from '../../img/bob.jpg';
+import song from '../../data/Billy Idol - Speed.mp3';
 
 const Card = () => {
+
+    const [playing, setPlaying] = useState(false);
+    const [mute, setMute] = useState(false);
+
+
+
     return (
         <section className='container'>
             <article className='card'>
@@ -17,10 +25,15 @@ const Card = () => {
                     </div>
 
                     <div className='content-duration'>
+
+                        <audio controls src={song} type="audio/mpeg" id='song' ></audio>
+
                         <div className='content-duration-time'>
                             <h3>0:46</h3>
                             <h3>3:52</h3>
                         </div>
+
+                        {/*<input type="range" value={20} min='0' max='100' className='progres__range' id='progresRange' />*/}
 
                         <span className="line-main"></span>
                         <span className="line-moving"></span>
@@ -31,10 +44,26 @@ const Card = () => {
                             <FaBackward className='icon-m ' />
                             <span className='neon__ring p-22 t2 '></span>
                         </div>
-                        <div className='content-controls-container'>
-                            <FaPlay className='icon-xl' />
-                            <span className='neon__ring p-26 t3'></span>
+
+                        <div className='content-controls-container' id="playButton">
+
+                            {playing == false ?
+                                (
+                                    <div className='content-controls-container' onClick={() => setPlaying(true)} >
+                                        <FaPlay className='icon-xl' />
+                                        <span className='neon__ring p-26'></span>
+                                    </div>
+                                )
+                                :
+                                (
+                                    <div className='content-controls-container' onClick={() => setPlaying(false)} >
+                                        <FaPause className='icon-xl' />
+                                        <span className='neon__ring p-26'></span>
+                                    </div>
+                                )
+                            }
                         </div>
+
                         <div className='content-controls-container'>
                             <FaForward className='icon-m' />
                             <span className='neon__ring p-22 t1'></span>
