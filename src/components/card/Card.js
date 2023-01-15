cffffe1f1204be59f5633fdca8ada94d493c01f5
srdjan <srdjan.gr@gmail.com>
2023-01-15 01:30:03 +0100

    Custom controls add for Play/Pause, Mute, song Back, song Forward, Duration, and manual time skip

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,15 +1,110 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { FaForward, FaBackward, FaPlay, FaVolumeUp, FaVolumeMute, FaPause } from 'react-icons/fa';
+import React, { useEffect } from 'react'
+import { useState, useRef } from 'react'
+import { FaFastForward, FaFastBackward, FaPlay, FaVolumeUp, FaVolumeMute, FaPause } from 'react-icons/fa';
+import { songsData } from '../../data';
 
 import album from '../../img/bob.jpg';
-import song from '../../data/Billy Idol - Speed.mp3';
+
 
 const Card = () => {
 
-    const [playing, setPlaying] = useState(false);
+    const [songs, setSongs] = useState(songsData);
+    const [currentSong, setCurrentSong] = useState(songsData[2]);
+    const [isPlaying, setIsPlaying] = useState(false);
     const [mute, setMute] = useState(false);
+    const [volume, setVolume] = useState(1);
+    const [autoplay, setAutoplay] = useState(false);
 
+    const audioElement = useRef();
+    const clickRef = useRef();
+
+
+
+
+
+    // useEffect(() => {
+    //     onPlaying()
+    // }, [isPlaying])
+
+    const playPause = () => {
+        if (isPlaying) {
+            setIsPlaying(false);
+            audioElement.current.pause();
+        } else {
+            setIsPlaying(true);
+            audioElement.current.play();
+        }
+    }
+
+    const muteFunction = () => {
+        if (mute) {
+            setMute(false);
+            audioElement.current.volume = 1;
+        } else {
+            setMute(true);
+            audioElement.current.volume = 0;
+        }
+    }
+
+
+    const changeSong = () => {
+
+        let index = songs.findIndex(x => x.title == currentSong.title);
+
+        if (index == 0) {
+            setCurrentSong(songs[songs.length - 1]);
+            audioElement.current.play();
+        } else {
+            setCurrentSong(songs[index - 1]);
+            audioElement.current.autoplay = true;
+        }
+    }
+
+
+    const renderTimeDuration = () => {
+
+        let fullTime = currentSong.length / 100;
+        let st = fullTime.toFixed(2)
+        let str = st.toString();
+
+        let currentTime = currentSong.current;
+
+        if (!currentTime) {
+            return (
+                <div className='content-duration-time'>
+                    <h3>0:00</h3>
+                    <h3>0:00</h3>
+                </div>
+            )
+        } else {
+            let sct = currentTime.toFixed(0)
+            let cut = str.split('.');
+
+            return (
+                <div className='content-duration-time'>
+                    <h3>0:{sct}</h3>
+                    <h3>{`${cut[0]}:${cut[1]}`}</h3>
+                </div>
+            )
+        }
+    }
+
+
+    const onPlaying = () => {
+        const duration = audioElement.current.duration;
+        const currentTime = audioElement.current.currentTime;
+
+        setCurrentSong({ ...currentSong, 'progress': currentTime / duration * 100, 'length': duration, 'current': currentTime, })
+    }
+
+
+    const changeProgress = (e) => {
+        let width = clickRef.current.clientWidth;
+        const offset = e.nativeEvent.offsetX;
+
+        const divprogress = offset / width * 100;
+        audioElement.current.currentTime = divprogress / 100 * currentSong.length;
+    }
 
 
     return (
@@ -20,43 +115,40 @@ const Card = () => {
 
                 <div className="content">
                     <div className='content-info'>
-                        <h1>Bob Marley</h1>
-                        <h2>Song</h2>
+                        <h1>{currentSong.artist}</h1>
+                        <h2>{currentSong.title}</h2>
                     </div>
 
                     <div className='content-duration'>
 
-                        <audio controls src={song} type="audio/mpeg" id='song' ></audio>
+                        <audio src={currentSong.path} type="audio/mpeg" ref={audioElement} onTimeUpdate={onPlaying} />
 
-                        <div className='content-duration-time'>
-                            <h3>0:46</h3>
-                            <h3>3:52</h3>
-                        </div>
+                        {renderTimeDuration()}
 
                         {/*<input type="range" value={20} min='0' max='100' className='progres__range' id='progresRange' />*/}
 
-                        <span className="line-main"></span>
-                        <span className="line-moving"></span>
+                        <span className="line-main" onClick={changeProgress} ref={clickRef}>
+                            <span className="line-moving" style={{ width: `${currentSong.progress + '%'}` }} ></span>
+                        </span>
                     </div>
 
                     <div className='content-controls'>
-                        <div className='content-controls-container'>
-                            <FaBackward className='icon-m ' />
+                        <div className='content-controls-container' onClick={() => changeSong()}>
+                            <FaFastBackward className='icon-m ' />
                             <span className='neon__ring p-22 t2 '></span>
                         </div>
 
                         <div className='content-controls-container' id="playButton">
-
-                            {playing == false ?
+                            {isPlaying == false ?
                                 (
-                                    <div className='content-controls-container' onClick={() => setPlaying(true)} >
+                                    <div className='content-controls-container' onClick={() => playPause()} >
                                         <FaPlay className='icon-xl' />
                                         <span className='neon__ring p-26'></span>
                                     </div>
                                 )
                                 :
                                 (
-                                    <div className='content-controls-container' onClick={() => setPlaying(false)} >
+                                    <div className='content-controls-container' onClick={() => playPause()} >
                                         <FaPause className='icon-xl' />
                                         <span className='neon__ring p-26'></span>
                                     </div>
@@ -64,8 +156,8 @@ const Card = () => {
                             }
                         </div>
 
-                        <div className='content-controls-container'>
-                            <FaForward className='icon-m' />
+                        <div className='content-controls-container' onClick={() => changeSong()}>
+                            <FaFastForward className='icon-m' />
                             <span className='neon__ring p-22 t1'></span>
                         </div>
                     </div>
@@ -76,16 +168,27 @@ const Card = () => {
 
                 <div className="sound__control">
                     <span className="sound__control-line-main"></span>
-                    <span className="sound__control-line-moving"></span>
+                    <span className="sound__control-line-moving" style={{ height: '10%' }}></span>
                 </div>
 
-                <div className="sound-icon">
-                    <FaVolumeUp className='icon-m' />
-                    <span className='neon__ring p-22'></span>
-                </div>
-
+                {
+                    mute == false ?
+                        (
+                            <div className="sound-icon" onClick={() => muteFunction()}>
+                                <FaVolumeUp className='icon-m' />
+                                <span className='neon__ring p-22'></span>
+                            </div>
+                        )
+                        :
+                        (
+                            <div className="sound-icon" onClick={() => muteFunction()}>
+                                <FaVolumeMute className='icon-m' />
+                                <span className='neon__ring p-22'></span>
+                            </div>
+                        )
+                }
             </article>
-        </section>
+        </section >
     )
 }
 
---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const songsData = [
+    {
+        id: 1,
+        title: 'Speed',
+        artist: 'Billy Idol',
+        path: './data/songs/Billy Idol - Speed.mp3'
+    },
+    {
+        id: 2,
+        title: 'The Road To Hell',
+        artist: 'Cris Rea',
+        path: './data/songs/Cris Rea - The Road To Hell.MP3'
+    },
+    {
+        id: 3,
+        title: 'Slatka mala',
+        artist: 'Fleke',
+        path: './data/songs/Fleke - Slatka mala.mp3'
+    },
+    {
+        id: 4,
+        title: 'Heroj Ulice',
+        artist: 'Prljavo Kazaliste',
+        path: './data/songs/Prljavo Kazaliste - Heroj Ulice.mp3'
+    }
+]
